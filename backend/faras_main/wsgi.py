826916e176f1253adc95673fd04097e837273e23
mshayar <studywell5253@gmail.com>
2026-09-14 02:24:54 +0300

    deployment process 3.0

--- backend/faras_main/wsgi.py
+++ backend/faras_main/wsgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'faras_main.settings')
+settings_module = 'faras_main.deployment_settings' if 'RENDER_EXTERNAL_HOSTNAME' in os.environ else 'faras_main.settings'
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
 application = get_wsgi_application()
