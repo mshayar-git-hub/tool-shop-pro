99a9c930a3df057deb669030214819c1fb0a53ef
mshayar <studywell5253@gmail.com>
2026-09-14 16:39:03 +0300

    deploying process 7.0

--- backend/faras_main/deployment_settings.py
+++ backend/faras_main/deployment_settings.py
@@ -21,9 +21,9 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-# CORS_ALLOWED_ORIGINS = [
-#     'https://docker-trial-1.onrender.com',
-# ]
+CORS_ALLOWED_ORIGINS = [
+    'https://buildverse-jpcr.onrender.com',
+]
 
 STORAGES = {
     "default" : {
